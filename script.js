d3fa1e903514074a84c7bd83837637e8e597ac0c
Samira Vaeidi <131511478+Samira-V@users.noreply.github.com>
2025-06-01 11:41:41 +0330

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ convert.addEventListener('click', () => {
     switch (TemperatureFrom.value) {
         case "Fahrenheit":
             const FahrenheitToCelsius = ((TemperatureFromInput.value - 32) * 5 / 9).toFixed(2)
-            const FahrenheitToKelvin = ((TemperatureFromInput.value + 459.67) * 5 / 9).toFixed(2)
+           const FahrenheitToKelvin = ((parseInt(TemperatureFromInput.value) + 459.67) * 5 / 9).toFixed(2)
             result.innerHTML = `<span>${TemperatureFromInput.value} °F = ${FahrenheitToCelsius} °C </span> 
             <span>${TemperatureFromInput.value} °F = ${FahrenheitToKelvin} °K </span>
             `
             break;
         case "Celsius":
-            const CelsiusToKelvin = TemperatureFromInput.value + 273.15
-            console.log(CelsiusToKelvin)
+             const CelsiusToKelvin = parseInt(TemperatureFromInput.value) + 273.15
             const CelsiusToFahrenheit = (TemperatureFromInput.value * 9 / 5 + 32).toFixed(2)
             result.innerHTML = `<span>${TemperatureFromInput.value} °C = ${CelsiusToKelvin} °K </span> 
             <span>${TemperatureFromInput.value} °C = ${CelsiusToFahrenheit} °F </span>
